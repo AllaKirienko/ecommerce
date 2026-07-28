a5f2925268f3600bbdef03061c2ab62f08dd0466
Алла <alla@MacBook-Air-Alla.local>
2026-07-28 10:15:26 +0700

    Add main module

---
+++ src/ecommerce/main.py
@@ -0,0 +1,33 @@
+from ecommerce.classes import Category, Product
+
+
+def main():
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+    )
+
+    product2 = Product(
+        "Iphone 15",
+        "512GB, Gray space",
+        210000.0,
+        8,
+    )
+
+    category = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2],
+    )
+
+    print(category.name)
+    print(category.description)
+    print(len(category.products))
+    print(Category.category_count)
+    print(Category.product_count)
+
+
+if __name__ == "__main__":
+    main()
